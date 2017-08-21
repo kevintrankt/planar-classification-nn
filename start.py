ceeb7f473e260535d34afca0396a26cb05525518
Kevin Tran <kevin.tran.kt@gmail.com>
2017-08-21 12:54:19 -0700

    Initial commit and imports

---
+++ start.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+%matplotlib inline
+
+np.random.seed(1)
